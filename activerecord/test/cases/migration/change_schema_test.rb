5bfb92dbbca425b2b22e8c22b59636d54b62bf5f
Yves Senn <yves.senn@gmail.com>
2014-12-18 10:07:23 +0100

    `force: :cascade` to recreate tables referenced by foreign-keys.

--- activerecord/test/cases/migration/change_schema_test.rb
+++ activerecord/test/cases/migration/change_schema_test.rb
@@ -415,5 +415,35 @@ module ActiveRecord
         yield
       end
     end
+
+    if ActiveRecord::Base.connection.supports_foreign_keys?
+      class ChangeSchemaWithDependentObjectsTest < ActiveRecord::TestCase
+        self.use_transactional_fixtures = false
+
+        setup do
+          @connection = ActiveRecord::Base.connection
+          @connection.create_table :trains
+          @connection.create_table(:wagons) { |t| t.references :train }
+          @connection.add_foreign_key :wagons, :trains
+        end
+
+        teardown do
+          [:wagons, :trains].each do |table|
+            @connection.drop_table(table) if @connection.table_exists?(table)
+          end
+        end
+
+        def test_create_table_with_force_cascade_drops_dependent_objects
+          # can't re-create table referenced by foreign key
+          assert_raises(ActiveRecord::StatementInvalid) do
+            @connection.create_table :trains, force: true
+          end
+
+          # can recreate referenced table with force: :cascade
+          @connection.create_table :trains, force: :cascade
+          assert_equal [], @connection.foreign_keys(:wagons)
+        end
+      end
+    end
   end
 end
